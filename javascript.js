7e688e8973f6339729dfb1a24f0694edd6216e89
dimzles <dimzlestv@gmail.com>
2022-10-07 17:33:27 +0100

    Add HTML & CSS to create calculator gui as well as inital operator functions

---
+++ javascript.js
@@ -0,0 +1,26 @@
+//Operation functions
+function add(num1, num2) {
+    return num1 + num2;
+};
+
+function subtract(num1, num2) {
+    return num1 - num2;
+};
+
+function multiply(num1, num2) {
+    return num1 * num2;
+};
+
+function divide(num1, num2) {
+    return num1 / num2;
+};
+
+function operate(operator, num1, num2) {
+    if (operator === add) return add(num1, num2);
+    if (operator === subtract) return subtract(num1, num2);
+    if (operator === multiply) return multiply(num1, num2);
+    if (operator === divide) {
+        if (num2 === 0) return `Nope`;
+        return divide(num1, num2);
+    }
+};
