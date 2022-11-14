a80355501425f3a40eb3fee72019bd44a6f2db3f
dimzles <dimzlestv@gmail.com>
2022-11-14 20:13:47 +0000

    Add full calculator functionality

--- javascript.js
+++ javascript.js
@@ -1,47 +1,91 @@
 let displayValue = 0;
+let firstNumber = null;
+let secondNumber = null;
+let operator = null;
+
 const display = document.getElementById('display-main');
 const btns = document.querySelectorAll('button');
 const clearBtn = document.getElementById('clearBtn');
 const deleteBtn = document.getElementById('deleteBtn');
+const equalsBtn = document.getElementById('equalsBtn');
+const decimalBtn = document.getElementById('decimalBtn');
 
 for (let i = 0; i < btns.length; i++) {
         btns[i].addEventListener('click', () => {
             if (btns[i].id === 'btn-num') {
-                if(displayValue === 0){
-                    displayValue = btns[i].textContent;
+                if(displayValue === 0 && btns[i].textContent === "0") {
+                    displayValue = 0;
                     updateDisplay();
-                } else
-                    displayValue += btns[i].textContent;
-                    updateDisplay();
-                    console.log(displayValue);
+                } else if (displayValue === 0 || displayValue === '0') {
+                        displayValue = btns[i].textContent;
+                        updateDisplay();
+                } else {
+                    display.textContent += btns[i].textContent;
+                }
+            }
+            if (btns[i].id === 'btn-op') {
+                handleOperator(btns[i].textContent);
             }
         })
     };
 
-clearBtn.addEventListener('click', () => {
-    clearDisplay();
+decimalBtn.addEventListener('click', () => {
+    inputDecimal();
 })
+
+equalsBtn.addEventListener('click', () => {
+    evaluate();
+});
+
+clearBtn.addEventListener('click', () => {
+    decimalBtn.removeAttribute('disabled');
+    clearDisplay();
+});
 
 deleteBtn.addEventListener('click', () => {
     deleteDigit();
-})
+});
+
+function handleOperator(input) {
+    if (operator !== null) evaluate();
+    firstNumber = display.textContent;
+    operator = input;
+    displayValue = 0;
+    updateDisplay();
+}
+
+function evaluate() {
+    if (operator === null) return;
+    if (operator === '÷' && display.textContent === '0') {
+        alert('Dividing by zero is bad.')
+        return;
+    }
+    secondNumber = display.textContent;
+    display.textContent = roundNumber(operate(operator, firstNumber, secondNumber));
+    operator = null;
+}
+
+function inputDecimal() {
+    displayValue += decimalBtn.textContent;
+    updateDisplay();
+    decimalBtn.setAttribute('disabled', '1');
+}
 
 function deleteDigit() {
     if(displayValue.length <= 1) {
         displayValue = 0;
-        display.textContent = displayValue;
         updateDisplay();
     } else
         display.textContent = display.textContent.toString().slice(0, -1);
         displayValue = display.textContent;
-        updateDisplay();
         console.log(displayValue)
 }
 
 function clearDisplay() {
     displayValue = 0;
+    firstNumber = null;
+    secondNumber = null;
     updateDisplay();
-    console.log(displayValue)
 }
 
 function updateDisplay() {
@@ -51,6 +95,10 @@ function updateDisplay() {
         displayValue = displayValue.substring(0, 14);
     }
 
+}
+
+function roundNumber(num) {
+    return Math.round(num * 1000) / 1000;
 }
 
 //Operation functions
@@ -71,10 +119,12 @@ function divide(num1, num2) {
 };
 
 function operate(operator, num1, num2) {
-    if (operator === add) return add(num1, num2);
-    if (operator === subtract) return subtract(num1, num2);
-    if (operator === multiply) return multiply(num1, num2);
-    if (operator === divide) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+    if (operator === '+') return add(num1, num2);
+    if (operator === '−') return subtract(num1, num2);
+    if (operator === '×') return multiply(num1, num2);
+    if (operator === '÷') {
         if (num2 === 0) return `Nope lol`;
         return divide(num1, num2);
     }
